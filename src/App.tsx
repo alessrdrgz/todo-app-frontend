cb8e85effbc6677cd04f80c9169c97e108e85980
alessrdrgz <alessrodrgz@gmail.com>
2022-05-27 13:53:30 +0200

    app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import AddTask from "./pages/AddTask";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
+        <Route path={"list"} element={<Home />} />
+        <Route path={"historic"} element={<Home completed />} />
+        <Route path={"add-task"} element={<AddTask />} />
+        <Route path={"id/:id"} element={<Home />} />
+        <Route index element={<Navigate to="/list" replace />} />
+        <Route path="*" element={<Navigate to="/list" replace />} />
       </Route>
     </Routes>
   );
