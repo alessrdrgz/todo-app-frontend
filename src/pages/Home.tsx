68144cf3871f2a3620824a384e47a8cea14e1e57
alessrdrgz <alessrodrgz@gmail.com>
2022-05-27 13:52:55 +0200

    home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,42 @@
 import { Typography } from "antd";
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
+import TodoItemDisplay from "../components/TodoItemDisplay";
+import TodoList from "../components/TodoList";
+import { TodoItem } from "../interfaces/TodoItem";
 import "../styles/Home.css";
+import TodoApiRequests from "../tools/TodoApiRequests";
+interface HomeProps {
+  completed?: boolean;
+}
 
-const { Title } = Typography;
-export default function Home() {
+export default function Home({ completed }: HomeProps) {
+  const [todos, setTodos] = useState<TodoItem[]>([]);
+
+  const { id } = useParams();
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    TodoApiRequests.search({ completed: completed ? true : false }).then(
+      (r) => {
+        if (Array.isArray(r)) {
+          setTodos(r);
+        }
+      }
+    );
+  }, [completed]);
+
   return (
     <section className="home-container">
-      <Title>TODO List</Title>
+      <Typography.Title>{t("title", { ns: "home" })}</Typography.Title>
+      <TodoList selectedId={id ? +id : null} todos={todos} />
+      {id && (
+        <AnimatePresence>
+          <TodoItemDisplay id={+id} key="item" />
+        </AnimatePresence>
+      )}
     </section>
   );
 }
