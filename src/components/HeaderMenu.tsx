82eb6d5537c90af5474048c1a95a93f97f4dcb82
alessrdrgz <alessrodrgz@gmail.com>
2022-05-27 13:51:49 +0200

    header menu component

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,22 +1,31 @@
 import { Menu } from "antd";
 import { ItemType } from "antd/lib/menu/hooks/useItems";
+import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
 
 export default function HeaderMenu() {
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   const MenuItems: ItemType[] = [
     {
       key: "list",
-      label: "Lista",
+      label: t("list", { ns: "header-menu" }),
       onClick: () => navigate(`/list`),
     },
     {
       key: "historic",
-      label: "Histórico",
+      label: t("historic", { ns: "header-menu" }),
       onClick: () => navigate(`/historic`),
     },
   ];
 
-  return <Menu theme="dark" mode="horizontal" items={MenuItems} />;
+  return (
+    <Menu
+      theme="dark"
+      mode="horizontal"
+      items={MenuItems}
+      style={{ display: "inline-block", width: "auto" }}
+    />
+  );
 }
