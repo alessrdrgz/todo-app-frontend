8b1711a57fab7212698f3a61ecbe82b304f95d58
alessrdrgz <alessrodrgz@gmail.com>
2022-05-27 13:51:25 +0200

    layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,44 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
-import { Layout as LY } from "antd";
+import { Dropdown, Layout as LY, Menu, Space } from "antd";
 import HeaderMenu from "./HeaderMenu";
+import { useTranslation } from "react-i18next";
 
 const { Header, Content } = LY;
 
 export default function Layout() {
+  const { t, i18n } = useTranslation();
+
+  const menu = (
+    <Menu
+      items={[
+        {
+          key: "1",
+          label: t("spanish", { ns: "languages" }),
+          onClick: () => i18n.changeLanguage("es"),
+        },
+        {
+          key: "2",
+          label: t("english", { ns: "languages" }),
+          onClick: () => i18n.changeLanguage("en"),
+        },
+      ]}
+    />
+  );
+
   return (
     <main className="App">
       <LY className="layout">
         <Header>
-          <div className="logo" />
           <HeaderMenu />
+          <Dropdown overlay={menu}>
+            <a
+              onClick={(e) => e.preventDefault()}
+              href="blank"
+              style={{ position: "absolute", right: "20px" }}
+            >
+              <Space>{t(i18n.language, { ns: "languages" })}</Space>
+            </a>
+          </Dropdown>
         </Header>
         <Content style={{ backgroundColor: "none" }}>
           <Outlet />
